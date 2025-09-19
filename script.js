89031c3048285133c26d2a193671ae6d08417847
lyanta-l <lyanta.why@gmail.com>
2025-09-19 10:49:27 +0800

    feat:添加了加减乘除函数

---
+++ script.js
@@ -0,0 +1,23 @@
+const add = function (last, next) {
+    last = +last;
+    next = +next;
+    return last + next;
+}
+
+const subtract = function (last, next) {
+    last = +last;
+    next = +next;
+    return last - next;
+}
+
+const multiply = function (last, next) {
+    last = +last;
+    next = +next;
+    return last * next;
+}
+
+const divide = function (last, next) {
+    last = +last;
+    next = +next;
+    return last / next;
+}
