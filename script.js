0960fbdbf12f9e3232e5171451c334c8d06b2c41
lyanta-l <lyanta.why@gmail.com>
2025-09-19 11:20:23 +0800

    feat:添加了operate函数通过操作符调用加减乘除函数

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ const divide = function (last, next) {
     next = +next;
     return last / next;
 }
+
+const operate = function (operator, last, next) {
+    switch (operator) {
+        case "add":
+            return add(last, next);
+        case "subtract":
+            return subtract(last, next);
+        case "multiply":
+            return multiply(last, next);
+        case "divide":
+            return divide(last, next);
+        default:
+            console.log("somthing wrong");
+    }
+}
