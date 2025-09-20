c09511ac6018058fb2d5c5f3569a71a5513d96ca
lyanta-l <lyanta.why@gmail.com>
2025-09-20 16:42:44 +0800

    feat:增加了数字和运算符的事件，实现了加减乘除

--- script.js
+++ script.js
@@ -36,3 +36,48 @@ const operate = function (operator, last, next) {
             console.log("somthing wrong");
     }
 }
+
+let last = '';
+let display = '';
+let operator = '';
+let shouldResetDisplay = false;
+
+const displayScreen = document.querySelector(".display");
+const numberButtons = document.querySelectorAll(".number");
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (shouldResetDisplay) {//如果进行了运算在点击数字先清空
+            display = '';
+            shouldResetDisplay = false;
+        }
+        display += button.textContent;//存储输入的数字在display里并显示
+        displayScreen.textContent = display;
+    })
+});
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log(button.id);
+        if (operator !== '') {
+            equalButton.click(); // 直接触发等号的点击事件来完成运算！
+        }
+        last = display;
+        operator = button.id;
+        shouldResetDisplay = true;
+    });
+});
+
+const equalButton = document.querySelector(".equal");
+equalButton.addEventListener("click", () => {
+    if (last === '' || operator === '') return;
+    if(shouldResetDisplay) return;
+    let next = display;
+    last = operate(operator, last, next);
+    displayScreen.textContent = last;
+    display = last;
+    operator = '';
+
+    shouldResetDisplay = true;
+});
